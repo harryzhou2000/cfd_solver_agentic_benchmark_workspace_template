058801fbb4c962f3ce9a8207ef24049b5c2ca54f
CFD Agent <cfd_agent@benchmark.local>
2026-08-28 11:01:00 +0800

    results: curate sonnet46-01 build artifacts

--- solver/build/CMakeFiles/cfd_solver.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for cfd_solver.
-# This may be replaced when dependencies are built.
--- solver/build/CMakeFiles/cfd_solver.dir/src/MeshBuilder.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/cfd_solver.dir/src/MeshBuilder.cpp.o: \
- /workspace/solver/src/MeshBuilder.cpp /usr/include/stdc-predef.h \
- /workspace/solver/src/MeshBuilder.hpp
